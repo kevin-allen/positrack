f159c4877fa68fef76d0dc27fa9a61a2bdfbd60e
Kevin Allen <allen@uni-heidelberg.de>
2015-03-20 13:13:06 +0100

    small nothing

--- src/callback.c
+++ src/callback.c
@@ -67,7 +67,7 @@ int init_window()
 
   // show a nice icon
   char* icon_file_name;
-  file_name = "radar-icon.png";
+  file_name = "radar_icon.png";
   icon_file_name=g_strdup_printf("%s/%s",DATADIR,file_name);
   gtk_window_set_icon(GTK_WINDOW(widgets.window), create_pixbuf(icon_file_name));
 
